855bd3c288112907a0863212c407bbef6f4de8f4
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2018-09-20 12:38:04 +0200

    switched to wpf app

---
+++ MemoryGame-1B/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MemoryGame_1B
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
