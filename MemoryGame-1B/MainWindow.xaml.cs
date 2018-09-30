5acccb85b03d27bdd762f81d8153e866af48e9e4
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2018-09-30 20:35:14 +0200

    cleaned up

--- MemoryGame-1B/MainWindow.xaml.cs
+++ MemoryGame-1B/MainWindow.xaml.cs
@@ -8,5 +8,25 @@ namespace MemoryGame_1B
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private void NewGame(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void LoadGame(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void HighScores(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Help(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
