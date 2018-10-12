48f7a04eac8ecd3251cbc466b9844b6be6bb535c
Sjouke Yntema <suyntema@live.nl>
2018-10-12 11:03:48 +0200

    New CardData Type
    Add:
    - New CardData to hold card ID, front- and back image and event for when the card is clicked

---
+++ MemoryGame-1B/Src/CardData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace MemoryGame_1B
+{
+    internal struct CardData
+    {
+        // Event for when card is clicked, supplying the card
+
+        public readonly int ID;
+        public readonly Image FrontImage;
+        public readonly Image BackImage;
+
+        public CardData(int id, Image frontImage, Image backImage)
+        {
+            ID = id;
+            FrontImage = frontImage;
+            BackImage = backImage;
+        }
+    }
+}
