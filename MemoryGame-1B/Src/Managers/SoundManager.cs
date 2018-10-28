d4309e8a403b8e64ece12199c612d5069e3ca580
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2018-10-28 13:06:26 +0100

    Random sound effect

---
+++ MemoryGame-1B/Src/Managers/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Windows.Media;
+
+namespace MemoryGame_1B.Managers
+{
+    /// <summary>
+    /// Handles the playing of sound
+    /// </summary>
+    public static class SoundManager
+    {
+        /// <summary>
+        /// All the sound players
+        /// </summary>
+        private static readonly List<SoundPlayer> SoundPlayers = new List<SoundPlayer>();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        static SoundManager() => LoadSounds();
+
+        /// <summary>
+        /// Loads The Sounds
+        /// </summary>
+        private static void LoadSounds()
+        {
+            var uri = new Uri("../../SoundEffects/Zombie", UriKind.Relative).ToString();
+
+            foreach (var file in Directory.GetFiles(uri))
+            {
+                var soundPlayer = new SoundPlayer(file);
+                SoundPlayers.Add(soundPlayer);
+            }
+        }
+
+        /// <summary>
+        /// Plays a random sound
+        /// </summary>
+        public static void PlayRandom() => SoundPlayers.Random().Play();
+    }
+}
