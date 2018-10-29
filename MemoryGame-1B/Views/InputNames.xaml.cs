5dd76f0395794346d32c0e2c1221828913edd61c
Sjouke Yntema <suyntema@live.nl>
2018-10-29 13:15:16 +0100

    New new InputNames page
    Added:
    
    Modified:
    
    Deleted:

--- MemoryGame-1B/Views/InputNames.xaml.cs
+++ MemoryGame-1B/Views/InputNames.xaml.cs
@@ -1,75 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using MemoryGame_1B.SaveData;
-using Microsoft.Win32;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace MemoryGame_1B.Views
 {
-    /// <inheritdoc cref="Page" />
     /// <summary>
-    /// Interaction logic for Main.xaml
+    /// Interaction logic for InputNames.xaml
     /// </summary>
-    public partial class Main
+    public partial class InputNames : Page
     {
-        /// <inheritdoc />
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public Main() => InitializeComponent();
-
-        /// <summary>
-        /// OnClickListener
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void NewGame(object sender, RoutedEventArgs e) =>
-            MainWindow.Instance.Content = new NewGame(GridSize.Normal);
-
-        /// <summary>
-        /// OnClickListener
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void LoadGame(object sender, RoutedEventArgs e)
-        {
-            var openFileDialog = new OpenFileDialog
-            {
-                DefaultExt = ".json",
-                Filter = "Json documents (.json)|*.json"
-            };
-
-            var showDialog = openFileDialog.ShowDialog();
-            if (showDialog != true) return;
-
-            var fileName = openFileDialog.FileName;
-            if (!File.Exists(fileName)) return;
-            var saveData = SaveData.SaveData.Load(fileName);
-
-            MainWindow.Instance.Content = new NewGame(saveData);
-        }
-
-        /// <summary>
-        /// OnClickListener
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void HighScores(object sender, RoutedEventArgs e) => MainWindow.Instance.Content = new HighScores();
-
-        /// <summary>
-        /// OnClickListener
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Help(object sender, RoutedEventArgs e) => MainWindow.Instance.Content = new Help();
-
-
-        /// <summary>
-        /// OnClickListener
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Online(object sender, MouseButtonEventArgs e) => MainWindow.Instance.Content = new Online();
+        public InputNames() => InitializeComponent();
+        
     }
 }
